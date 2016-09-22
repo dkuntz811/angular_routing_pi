be340dd4ba64b2e01703bd6d8819ec22ca3a2107
Dave Kuntz <dkuntz811@gmail.com>
2016-09-22 14:49:01 -0500

    routes created

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,22 @@
 //[] below is the dependency injection array
-var myApp = angular.module('myApp', []);
+//brings in Angular Route   ngRoute is a variable
+var myApp = angular.module('myApp', ['ngRoute']);
+//configure myApp
+myApp.config(['$routeProvider', function($routeProvider){
+	$routeProvider.
+			when('/home', {
+				templateUrl: '/views/partials/home.html'
+			}).
+			when('/cats', {
+				templateUrl: '/views/partials/cats.html'
+			}).
+			when('/lizards', {
+				templateUrl: '/views/partials/lizards.html'
+			}).
+			otherwise({
+				redirectTo: '/home'
+			});
+}]);
 
 
 
